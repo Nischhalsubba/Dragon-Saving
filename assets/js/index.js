304b304cdc9403d1ac005085dfb3c215fe39d0c6
Nischhal Subba <hinischalsubba@gmail.com>
2020-12-27 15:05:51 +0545

    design of the landing page

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+var glideService = new Glide('.glideservice', {
+  type: 'carousel',
+  autoplay: 3500,
+  perView: 3
+});
+
+glideService.mount();
---
+++ js/app.js
@@ -0,0 +1,1 @@
+var glideService=new Glide(".glideservice",{type:"carousel",autoplay:3500,perView:3});glideService.mount();
